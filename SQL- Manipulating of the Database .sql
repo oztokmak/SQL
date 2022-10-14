1d40c458cceb47239ebdaa44a3d321348d3d0b8e
Ömer Turan ÖZTOKMAK <65924870+turanoztokmak@users.noreply.github.com>
2022-10-14 11:37:31 +0300

    Manipulating of the Database

---
+++ SQL- Manipulating of the Database .sql
@@ -0,0 +1,22 @@
+﻿--Select
+
+--ANSII
+
+Select ContactName Adi, CompanyName SirketAdi, City Sehir from Customers
+
+Select * From Customers where City = 'London'
+Select * From Products where CategoryID=1 or CategoryID=3
+Select * From Products where CategoryID=1 and UnitPrice>=10
+Select * From Products order by UnitPrice asc --ascending --descending
+Select * From Products order by CategoryID, ProductName
+Select count(*) from Products
+Select CategoryID, count(*) From Products where UnitPrice>20 group by CategoryID having count(*)<10
+
+--JOIN
+
+Select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName From Products inner join Categories on Products.CategoryID = Categories.CategoryID
+
+--DTO Data Transformation Object
+
+Select * From Products p  left join [Order Details] od on p.ProductID = od.ProductID
+Select * From Customers c  left join Orders o on c.CustomerID = o.CustomerID where o.CustomerID is null
