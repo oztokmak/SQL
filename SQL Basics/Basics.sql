23df6b6e959a80edac578c6c809d8602841e6d3d
Ömer Turan ÖZTOKMAK <114072826+oztokmak@users.noreply.github.com>
2022-10-23 01:30:02 +0300

    Update Basics.sql

--- SQL Basics/Basics.sql
+++ SQL Basics/Basics.sql
@@ -17,3 +17,7 @@ select avg(rental_rate) from film;
 select count(title) from film where title ~~'C%';
 select max (length) from film where rental_rate = 0.99;
 select count (distinct replacement_cost)  from film where length > 150;
+select rating, count(*) from film group by rating;
+select replacement_cost, count(*) from film group by replacement_cost having count(*)>50;
+select store_id, count(*) from customer group by store_id;
+select country_id,count(city) from city group by country_id order by count(city) desc limit 1;
