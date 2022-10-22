d923959b45386234071f05e33e52aa71205bb236
Ömer Turan ÖZTOKMAK <114072826+oztokmak@users.noreply.github.com>
2022-10-23 01:40:38 +0300

    Update Basics.sql

--- SQL Basics/Basics.sql
+++ SQL Basics/Basics.sql
@@ -1,6 +1,8 @@
+-- Between & In
 select * from film where replacement_cost between 12.99 and 16.99;
 select first_name,last_name from actor where first_name in('Penelope', 'Nick', 'Ed');
 select * from film where rental_rate in(0.99, 2.99, 4.99) and replacement_cost in (12.99, 15.99, 29.99);
+-- Like & ILike & Discint & Count 
 select country from country where country ~~ 'A%a';
 select country from country where country ~~ '_______%n';
 select title from film where title ~~* '%t%t%t%t';
@@ -10,6 +12,7 @@ select count(replacement_cost) from film;
 select count(title) from film where title ~~* 't%' and rating ='G';
 select count (country) from country where country ~~* '_____';
 select count (city) from city where city ~~'R%r';
+-- Oder By & Limit & Offset & Aggregate & Group By & Having 
 select title from film where title ~~'%n' order by length desc limit 5;
 select title from film where title ~~'%n' order by length asc offset 5 limit 5;
 select * from customer where store_id=1 order by last_name desc limit 4;
